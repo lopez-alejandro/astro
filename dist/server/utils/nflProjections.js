d1ecc143971f4367b582f8e21be8355946a7596b
Alejandro Lopez <lope058@usc.edu>
2017-07-15 15:22:11 -0700

    first commit

---
+++ dist/server/utils/nflProjections.js
@@ -0,0 +1,5 @@
+/**
+ * Downloads player projections from the NFL website and converts them
+ * into a CSV file to be consumed later for our calculations
+ */
+"use strict";
---
+++ server/utils/nflProjections.js
@@ -0,0 +1,4 @@
+/**
+ * Downloads player projections from the NFL website and converts them
+ * into a CSV file to be consumed later for our calculations
+ */
