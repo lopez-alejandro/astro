aac885b6f06fc14145bcd2c3ee13603836f6f514
Alejandro Lopez <lope058@usc.edu>
2017-09-02 09:47:28 -0700

    hello

--- routes/index.js
+++ routes/index.js
@@ -13,16 +13,18 @@ var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+
+
+
+  res.render('index');
+});
+
+router.get('/data', async function(req, res, next) {
   await get_nfl_projections();
   await get_espn_projections();
   await get_cbs_projections();
   await get_fp_projections();
 
-
-  res.send('hi');
-});
-
-router.get('/data', async function(req, res, next) {
   exec('python server/utils/sanitizeProjections.py', function(error, stdout, stderr) {
     if(error) console.log(error);
 
@@ -42,23 +44,36 @@ router.get('/data', async function(req, res, next) {
 
 });
 
-router.get('/prepareData', async function(req, res, next) {
+router.get('/prepareDraft', async function(req, res, next) {
   await readData();
   res.send('loaded data');
 });
 
-router.get('/nextDraft', async function(req, res, next) {
+router.post('/nextDraft', async function(req, res, next) {
+  /*
   let teamObj = {
     qb: 2,
-    rb: 0,
-    wr: 0,
-    te: 0,
-    k: 0,
-    def: 0
+    rb: 2,
+    wr: 2,
+    te: 1,
+    flex: 1,
+    k: 1,
+    def: 1,
+    bench: {
+      qb: 1,
+      rb: 1,
+      wr: 1,
+      te: 1,
+      def: 0
+    }
   };
   let draftedPlayers = ['Aaron Rodgers', 'David Johnson', 'Le\'Veon Bell'];
+  */
+  //let draftedPlayers = [];
 
-
+  let teamObj = req.body.team ? req.body.team : {};
+  let draftedPlayers = req.body.draftedPlayers ? req.body.draftedPlayers : [];
+  console.log(teamObj);
   const nextPlayer = await getNextPrediction(teamObj, draftedPlayers);
   res.send(nextPlayer);
 });
--- server/utils/draft.js
+++ server/utils/draft.js
@@ -9,6 +9,18 @@ let te = {};
 let k = {};
 let def = {};
 
+// weights used for calculated adjusted value of player
+// for our bench we do not care so much about minimizing risk and we care more
+// about getting sleepers.
+const weights = {
+  points: 1,
+  risk: -10,
+  adpDif: -5,
+  riskBench: -5,
+  adpDifBench: -10
+}
+
+
 /**
  * Helper function that reads the csv files and puts them into js objects
  * @return {[type]} [description]
@@ -88,6 +100,7 @@ export async function readData() {
     kPromise,
     defPromise
   ]);
+
 };
 
 /**
@@ -98,12 +111,16 @@ export async function readData() {
  * 	rb: 0 // amount of rbs you have
  * 	wr:
  * 	te:
+ * 	flex:
  * 	k:
  * 	def:
  * 	bench: {
  *  	qb:
  *  	rb:
  *  	wr:
+ *  	te:
+ *  	k:
+ *  	def:
  *  }
  * }
  * @param {[String]} draftedPlayers string of drafted players that should not be included in the search for the next best player
@@ -112,23 +129,34 @@ export async function readData() {
 export async function getNextPrediction(teamObj, draftedPlayers) {
   // we want to prioritize our starting players first and put less priority on kickers, tight ends, and defenses
   let highestValue;
+  console.log(teamObj.bench);
   if(teamObj.qb < 2 || teamObj.rb < 2 || teamObj.wr < 2) {
-    console.log("LOOKING FOR A QB");
     // draft based on highest value over replacement between qbs, rb, and wr
     let qbValue = await getHighestValue('qb', draftedPlayers);
     let rbValue = await getHighestValue('rb', draftedPlayers);
     let wrValue = await getHighestValue('wr', draftedPlayers);
 
-    console.log(rbValue);
+    // apply the weights based on risk (std dev) and adp difference (adp dif)
+    qbValue.value = (qbValue.value * weights.points) +
+                    (qbValue.risk * weights.risk) +
+                    (qbValue.adpDif * weights.adpDif);
+    rbValue.value = (rbValue.value * weights.points) +
+                    (rbValue.risk * weights.risk) +
+                    (rbValue.adpDif * weights.adpDif);
+
+    wrValue.value = (wrValue.value * weights.points) +
+                    (wrValue.risk * weights.risk) +
+                    (wrValue.adpDif * weights.adpDif);
+
     // we have to prevent from over drafting a position. If we  have filled up our slots then we ensure that we do not pick another player of the same position
     if(teamObj.qb == 2) {
-      qbValue.value = 0;
+      qbValue.value = -100000;
     }
     if(teamObj.rb == 2) {
-      rbValue.value = 0;
+      rbValue.value = -100000;
     }
     if(teamObj.wr == 2) {
-      wrValue.value = 0;
+      wrValue.value = -100000;
     }
 
     if(qbValue.value >= rbValue.value && qbValue.value >= wrValue.value) {
@@ -136,34 +164,121 @@ export async function getNextPrediction(teamObj, draftedPlayers) {
     } else if (rbValue.value >= qbValue.value && rbValue.value >= wrValue.value){
       highestValue = rbValue;
     } else if (wrValue.value >= rbValue.value && wrValue.value >= qbValue.value) {
-      highestValue = rbValue;
+      highestValue = wrValue;
     }
+
     // get the highest valued qb
   } else if(teamObj.te < 1) {
-    teValue = getHighestValue('te', draftedPlayers);
+    teValue = await getHighestValue('te', draftedPlayers);
     highestValue = teValue;
+  } else if(teamObj.flex < 1) {
+    // for flex we consider rb, wr, and te
+    let rbValue = await getHighestValue('rb', draftedPlayers);
+    let wrValue = await getHighestValue('wr', draftedPlayers);
+    let teValue = await getHighestValue('te', draftedPlayers);
+
+    // apply the weights based on risk (std dev) and adp difference (adp dif)
+    teValue.value = (teValue.value * weights.points) +
+                    (teValue.risk * weights.risk) +
+                    (teValue.adpDif * weights.adpDif);
+
+    rbValue.value = (rbValue.value * weights.points) +
+                    (rbValue.risk * weights.risk) +
+                    (rbValue.adpDif * weights.adpDif);
+
+    wrValue.value = (wrValue.value * weights.points) +
+                    (wrValue.risk * weights.risk) +
+                    (wrValue.adpDif * weights.adpDif);
+    //
+    if (wrValue.value >= rbValue.value && wrValue.value >= teValue.value) {
+      highestValue = wrValue;
+    } else if (rbValue.value >= teValue.value && rbValue.value >= wrValue.value){
+      highestValue = rbValue;
+    } else if(teValue.value >= rbValue.value && teValue.value >= wrValue.value) {
+      highestValue = teValue;
+    }
   } else if(teamObj.k < 1 || teamObj.def < 1) {
-    kValue = getHighestValue('k', draftedPlayers);
-    defValue = getHighestValue('def', draftedPlayers);
+    let kValue = await getHighestValue('k', draftedPlayers);
+    let defValue = await getHighestValue('def', draftedPlayers);
+
+    kValue.value = (kValue.value * weights.points) +
+                    (kValue.risk * weights.risk) +
+                    (kValue.adpDif * weights.adpDif);
+    ///
+    defValue.value = (defValue.value * weights.points) +
+                    (defValue.risk * weights.risk) +
+                    (defValue.adpDif * weights.adpDif);
+
     // protect against over drafting
     if(teamObj.k == 1) {
-      kValue.value = 0;
+      kValue.value = -100000;
     }
     if(teamObj.def == 1) {
-      defValue.value = 0;
+      defValue.value = -100000;
     }
     if(defValue.value >= kValue.value) {
       highestValue = defValue;
     } else if(kValue.value >= defValue.value) {
       highestValue = kValue;
     }
-
   } else {
-    // now just draft our bench players
+    // now just draft our bench players=
+    if(teamObj.bench.qb < 1 || teamObj.bench.rb < 2 || teamObj.bench.wr < 2 || teamObj.bench.te < 1 ) {
+      // if all other positions are filled then we can go after extra defenses
+      // draft based on highest value over replacement between qbs, rb, and wr
+      let qbValue = await getHighestValue('qb', draftedPlayers);
+      let rbValue = await getHighestValue('rb', draftedPlayers);
+      let wrValue = await getHighestValue('wr', draftedPlayers);
+      let teValue = await getHighestValue('te', draftedPlayers);
+
+      // apply the weights based on risk (std dev) and adp difference (adp dif)
+      qbValue.value = (qbValue.value * weights.points) +
+                      (qbValue.risk * weights.riskBench) +
+                      (qbValue.adpDif * weights.adpDifBench);
+
+      rbValue.value = (rbValue.value * weights.points) +
+                      (rbValue.risk * weights.riskBench) +
+                      (rbValue.adpDif * weights.adpDifBench);
+
+      wrValue.value = (wrValue.value * weights.points) +
+                      (wrValue.risk * weights.riskBench) +
+                      (wrValue.adpDif * weights.adpDifBench);
+      ////
+      teValue.value = (teValue.value * weights.points) +
+                      (teValue.risk * weights.riskBench) +
+                      (teValue.adpDif * weights.adpDifBench);
+      ///
+      // we have to prevent from over drafting a position. If we  have filled up our slots then we ensure that we do not pick another player of the same position
+      if(teamObj.bench.qb == 1) {
+        qbValue.value = -100000;
+      }
+      if(teamObj.bench.rb == 2) {
+        rbValue.value = -100000;
+      }
+      if(teamObj.bench.wr == 2) {
+        wrValue.value = -100000;
+      }
+      if(teamObj.bench.te == 1) {
+        teValue.value = -100000;
+      }
+
+      if(qbValue.value >= rbValue.value && qbValue.value >= wrValue.value && qbValue.value >= teValue.value) {
+        highestValue = qbValue;
+      } else if (wrValue.value >= rbValue.value && wrValue.value >= qbValue.value && wrValue.value >= teValue.value) {
+        highestValue = wrValue;
+      } else if (rbValue.value >= qbValue.value && rbValue.value >= wrValue.value && rbValue.value >= teValue.value){
+        highestValue = rbValue;
+      } else if(teValue.value >= rbValue.value && teValue.value >= wrValue.value && teValue.value >= qbValue.value) {
+        highestValue = teValue;
+      }
+    } else if (teamObj.bench.def < 1){
+      let defValue = await getHighestValue('def', draftedPlayers);
+      highestValue = defValue;
+    }
   }
 
   if(highestValue) {
-    return highestValue.name;
+    return highestValue;
   } else {
     return null;
   }
@@ -199,7 +314,9 @@ export async function getHighestValue(position, draftedPlayers) {
   let player = {
     name: '',
     value: 0,
-    risk: 0
+    risk: 0,
+    adpDif: 0,
+    position: position
   };
   // search through all qbs sorted by value
   for(let index in obj) {
@@ -210,9 +327,9 @@ export async function getHighestValue(position, draftedPlayers) {
         player.name = obj[index].Player;
         player.value = max;
         player.risk = parseFloat(obj[index]['STD DEV']);
+        player.adpDif = parseFloat(obj[index]['ADP DIF']);
       }
     }
   }
-  console.log(player);
   return player;
 };
